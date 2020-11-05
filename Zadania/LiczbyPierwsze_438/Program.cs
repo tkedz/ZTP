7effdfe2cf291b06d2c8e2c4387df5c55db21c4d
tkedz <tomasz.kedziora97@gmail.com>
2020-11-05 15:45:32 +0100

    init

---
+++ Zadania/LiczbyPierwsze_438/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LiczbyPierwsze_438
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
