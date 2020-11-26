c558b541661dfa86437e0dd87b0ea2e01cc818de
tkedz <tomasz.kedziora97@gmail.com>
2020-11-26 15:42:52 +0100

    liczby znaczace poprawka

--- Zadania/LiczbyZnaczace_575/Program.cs
+++ Zadania/LiczbyZnaczace_575/Program.cs
@@ -1,45 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LiczbyZnaczace_575
 {
     class Program
     {
-        static bool checkIfSignifiant(int number)
+        static bool czyPierwsza(int a)
         {
-            int sum = 0;
-            int counter = 0;
-            double sqrt = Math.Sqrt(number);
-            for(int i=2; i < Math.Sqrt(number); i++)
+            if (a % 2 == 0)
+                return (a == 2);
+            for (int i = 3; i <= Math.Sqrt(a); i += 2)
             {
-                if(number % i == 0)
+                if (a % i == 0)
                 {
-                    sum += i;
-                    counter++;
+                    return false;
                 }
             }
-
-            double avg = (sum * 1.0) / counter;
-
-            if (avg <= sqrt) return true;
-            return false;
+            return true;
         }
         static void Main(string[] args)
         {
-            int t = int.Parse(Console.ReadLine());
+            List<int> liczbyZnaczace = new List<int>();
 
+            for(int i=2; i < Math.Sqrt(1000000000); i++)
+            {
+                if(czyPierwsza(i))
+                {
+                    liczbyZnaczace.Add(i * i);
+                }
+            }
+
+            int t = int.Parse(Console.ReadLine());
             while(t > 0)
             {
                 string[] tmp = Console.ReadLine().Split(' ');
-                int num1 = int.Parse(tmp[0]);
-                int num2 = int.Parse(tmp[1]);
+                int a = int.Parse(tmp[0]);
+                int b = int.Parse(tmp[1]);
 
-                int counter=0;
-                for(int i=num1; i<=num2; i++)
+                int counter = 0;
+                for(int i=0; i<liczbyZnaczace.Count; i++)
                 {
-                    if (checkIfSignifiant(i)) counter++;
+                    if (liczbyZnaczace[i] >= a && liczbyZnaczace[i] <= b) counter++;
+                    if (liczbyZnaczace[i] > b) break;
                 }
                 Console.WriteLine(counter);
-
                 t--;
             }
         }
