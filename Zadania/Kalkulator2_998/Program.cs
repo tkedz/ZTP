5db9341130207b8854658337b384cd8b6c460b93
tkedz <tomasz.kedziora97@gmail.com>
2021-01-21 14:10:14 +0100

    new

---
+++ Zadania/Kalkulator2_998/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator2_998
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            while ((input = Console.ReadLine()) != null)
+            {
+                string[] arr = input.Split(' ');
+                int a = int.Parse(arr[1]);
+                int b = int.Parse(arr[2]);
+
+                switch (arr[0])
+                {
+                    case "+":
+                        Console.WriteLine(a + b);
+                        break;
+                    case "-":
+                        Console.WriteLine(a - b);
+                        break;
+                    case "*":
+                        Console.WriteLine(a * b);
+                        break;
+                    case "/":
+                        Console.WriteLine(a / b);
+                        break;
+                    case "%":
+                        Console.WriteLine(a % b);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Zadania/Polowa_1011/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polowa_1011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            string s;
+            for(int i=0; i<t; i++)
+            {
+                s = Console.ReadLine();
+                string substr = s.Substring(0, s.Length / 2);
+                Console.WriteLine(substr);
+            }
+        }
+    }
+}
