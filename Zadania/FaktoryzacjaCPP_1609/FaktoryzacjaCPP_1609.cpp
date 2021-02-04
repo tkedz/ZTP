4ab3bf6f5b8282cdddc64ffe0dea92972ced9af9
tkedz <tomasz.kedziora97@gmail.com>
2021-02-04 14:05:36 +0100

    nowe

---
+++ Zadania/FaktoryzacjaCPP_1609/FaktoryzacjaCPP_1609.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+int dzielniki[8000001];
+int main()
+{
+
+    //sito erastotenesa
+    dzielniki[0] = 1;
+    for (int i = 1; i < 8000001; i++)
+        dzielniki[i] = i;
+
+    for (int i = 2; i * i <= 8000000; i++) {
+        if (dzielniki[i] == i) {
+            for (int j = i * i; j <= 8000000; j += i) {
+                if (dzielniki[j] == j)
+                    dzielniki[j] = i;
+            }
+        }
+    }
+
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        int num;
+        cin >> num;
+        cout << dzielniki[num];
+        num /= dzielniki[num];
+        while (num > 1) {
+            cout << "*" << dzielniki[num];
+            num /= dzielniki[num];
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
